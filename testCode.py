324d610b0aed2f2cc214338913767325e1bf1b06
Warren <36239435+warrenOdellette@users.noreply.github.com>
2018-03-03 15:05:38 -0700

    Add files via upload

---
+++ testCode.py
@@ -0,0 +1,18 @@
+
+def myPrint(y):
+    for ite in y:
+        print(ite)
+    return
+
+n = 3 #number of proteins analyzed 
+sec = 6
+charLength=len('sp|Q60892|OLF8_MOUSE        ')
+charLength2=len('PMYFFLANLSFVDICFTSTTVPKMLVNIQTQSKAITYADCISQMSVFLVFAELDNFLLA')
+
+with open('aminoacidSeq.txt') as f:
+    data=f.read().split('\n')
+
+myPrint(data)
+    
+
+
