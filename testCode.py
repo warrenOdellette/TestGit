a2cf07b59ff320390a861dad85f3b107338cb177
warrenOdellette <illyaseviel@gmail.com>
2018-03-03 15:20:29 -0700

    Made a new Branch
    Trial Branch

--- testCode.py
+++ testCode.py
@@ -1,19 +1,22 @@
 
+
 def myPrint(y):
     for ite in y:
         print(ite)
     return
 
-n = 3 #number of proteins analyzed 
+
+n = 3  # number of proteins analyzed
 sec = 6
-charLength=len('sp|Q60892|OLF8_MOUSE        ')
-charLength2=len('PMYFFLANLSFVDICFTSTTVPKMLVNIQTQSKAITYADCISQMSVFLVFAELDNFLLA')
+charLength = len('sp|Q60892|OLF8_MOUSE        ')
+charLength2 = len('PMYFFLANLSFVDICFTSTTVPKMLVNIQTQSKAITYADCISQMSVFLVFAELDNFLLA')
 
 with open('aminoacidSeq.txt') as f:
-    data=f.read().split('\n')
+    data = f.read().split('\n')
 
 myPrint(data)
 print("hello world")
-    
+print("over and done")
+
 
 
